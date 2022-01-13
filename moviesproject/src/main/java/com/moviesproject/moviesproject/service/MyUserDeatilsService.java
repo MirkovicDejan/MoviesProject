1c276cc4baa96a31397d9e7f119c3db03eb1a2c3
DESKTOP-0BRQHIJ\mirko <dejan.mirkovic@devlabs.at>
2022-01-13 08:56:18 +0100

    In this commit, add some security configuration and add dependency spring security

---
+++ moviesproject/src/main/java/com/moviesproject/moviesproject/service/MyUserDeatilsService.java
@@ -0,0 +1,21 @@
+package com.moviesproject.moviesproject.service;
+
+import com.moviesproject.moviesproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyUserDeatilsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return new User("Dejan","Dejan",new ArrayList<>());
+    }
+}
