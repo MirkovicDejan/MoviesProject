537adfb4e0749675bc68aabd927d0338090fb0eb
dejan.mirkovic@devlabs.at <dejan.mirkovic@devlabs.at>
2022-02-07 11:53:16 +0100

    tesr resolved conflic

--- moviesproject/src/test/java/com/moviesproject/moviesproject/controller/UserControllerTest.java
+++ moviesproject/src/test/java/com/moviesproject/moviesproject/controller/UserControllerTest.java
@@ -2,25 +2,19 @@ package com.moviesproject.moviesproject.controller;
 
 
 import com.moviesproject.moviesproject.JsonUtil.JsonUtil;
-import com.moviesproject.moviesproject.dto.DTOUser;
 import com.moviesproject.moviesproject.model.User;
 import com.moviesproject.moviesproject.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.web.bind.annotation.RestController;
-import javax.persistence.*;
-import java.util.*;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,35 +37,51 @@ class UserControllerTest {
 
     @Test
     void saveUser3() throws Exception {
-        
-		User user = new User();
-		User user2 = new User();
+
+        User user = new User();
+        User user2 = new User();
         user.setUserName("UsernameExample");
         user.setFirstName("FirstnameExample");
-        
-		
-		user.setLastName("LastExample");
+
+
+        user.setLastName("LastExample");
         user.setPassword("PassExamp");
         user.setPhoneNumber("PhoneExamp");
         user.setAdress("AdressExampp");
         user.setEmail("EmailExam");
-        
-		assertThat(user).isNotNull();
-		assertThat(user).isNotNull();
-		assertThat(user).isNotNull();
+
+        assertThat(user).isNotNull();
+        assertThat(user).isNotNull();
+        assertThat(user).isNotNull();
         assertThat(user).isExactlyInstanceOf(User.class);
-		
-		assertThat(user).isExactlyInstanceOf(User.class);
-		assertThat(user).isExactlyInstanceOf(User.class);
-		assertThat(user).isExactlyInstanceOf(User.class);
+
+        assertThat(user).isExactlyInstanceOf(User.class);
+        assertThat(user).isExactlyInstanceOf(User.class);
+        assertThat(user).isExactlyInstanceOf(User.class);
+    }
+
+    void saveUser() throws Exception {
+        User user3 = new User();
+        user3.setUserName("UsernameExample33");
+        user3.setFirstName("FirstnameExample444");
+        user3.setLastName("LastExample");
+        user3.setPassword("PassExamp");
+        user3.setPhoneNumber("PhoneExamp");
+        user3.setAdress("AdressExampp");
+        user3.setEmail("EmailExam@d");
+        assertThat(user3).isNotNull();
+        assertThat(user3).isExactlyInstanceOf(User.class);
 
         given(userService.saveUser(DTOUser.getInstanceDtoUser().entityUserToDTO(user))).willReturn(user);
 
         mockMvc.perform(post("/save-user").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(user)))
+                .content(JsonUtil.toJson(user3)))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.userName", is(user.getUserName())));
+                .andExpect((ResultMatcher) jsonPath("$.userName", is(user3.getUserName())));
+
+
+        // This is for source conflict
     }
 
     @Test
@@ -79,18 +89,4 @@ class UserControllerTest {
 		System.out.println("test");
     }
 
-    @Test
-    void one() {
-		System.out.println("test");
-    }
-
-    @Test
-    void delete() {
-		System.out.println("test");
-    }
-
-    @Test
-    void update() {
-		System.out.println("test");
-    }
 }
